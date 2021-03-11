fe1480b71bacfea6ea6964d71bdac47ca2fbd99d
lq2007 <lq2007lq@hotmail.com>
2021-03-11 11:21:14 +0800

    [E] 优化 Packet

--- src/main/java/lq2007/mcmod/isaacformc/common/util/serializer/ObjectPacketSerializer.java
+++ src/main/java/lq2007/mcmod/isaacformc/common/util/serializer/ObjectPacketSerializer.java
@@ -1,35 +1,55 @@
 package lq2007.mcmod.isaacformc.common.util.serializer;
 
+import lq2007.mcmod.isaacformc.common.util.serializer.network.IPacketReadable;
 import lq2007.mcmod.isaacformc.common.util.serializer.network.IPacketSerializable;
 import lq2007.mcmod.isaacformc.common.util.serializer.network.IPacketSerializer;
+import lq2007.mcmod.isaacformc.common.util.serializer.network.IPacketWriteable;
 import net.minecraft.network.PacketBuffer;
-import net.minecraftforge.eventbus.ASMEventHandler;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.javafmlmod.FMLModContainer;
-import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
-import net.minecraftforge.forgespi.language.IModInfo;
+import net.minecraftforge.forgespi.language.ModFileScanData;
+import org.objectweb.asm.Type;
 
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ObjectPacketSerializer<T extends IPacketSerializable> implements IPacketSerializer<T> {
 
     public static final ObjectPacketSerializer INSTANCE = new ObjectPacketSerializer();
-    public static final ArrayList<String> CLASS_LIST = new ArrayList<>();
+    public static final ArrayList<Class<?>> CLASS_LIST = new ArrayList<>();
 
     public static void collectClass() {
+        if (!CLASS_LIST.isEmpty()) return;
         ModContainer container = ModLoadingContext.get().getActiveContainer();
+        Set<Class<?>> set = new HashSet<>();
         if (container instanceof FMLModContainer) {
-            container.scanResults;
+            ModFileScanData data = container.scanResults;
+            for (ModFileScanData.ClassData classData : data.getClasses()) {
+                Type clazz = classData.clazz;
+                try {
+                    Class<?> aClass = ObjectPacketSerializer.class.getClassLoader().loadClass(clazz.getClassName());
+                    if (aClass.isInterface() || aClass.isAnnotation() || aClass.isEnum() || aClass.isAnonymousClass()) continue;
+                    if (Modifier.isAbstract(aClass.getModifiers())) continue;
+                    if (IPacketReadable.class.isAssignableFrom(aClass) || IPacketWriteable.class.isAssignableFrom(aClass)) {
+                        set.add(aClass);
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+        CLASS_LIST.addAll(set);
+        CLASS_LIST.sort(Comparator.comparing(Class::getName));
     }
 
     @Override
     public T read(PacketBuffer buffer) {
-        Class<T> aClass = (Class<T>) ClassSerializer.INSTANCE.read(buffer);
         try {
+            Class<T> aClass = (Class<T>) CLASS_LIST.get(buffer.readVarInt());
             T t = aClass.newInstance();
             t.read(buffer);
             return t;
@@ -40,7 +60,7 @@ public class ObjectPacketSerializer<T extends IPacketSerializable> implements IP
 
     @Override
     public PacketBuffer write(T item, PacketBuffer buffer) {
-        ClassSerializer.INSTANCE.write(item.getClass(), buffer);
+        buffer.writeVarInt(CLASS_LIST.indexOf(item.getClass()));
         item.write(buffer);
         return buffer;
     }
