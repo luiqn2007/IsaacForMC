68b2ee26f1275feab2939430fea84440be5bbd0a
luqin2007 <lq2007lq@hotmail.com>
2021-03-17 00:36:16 +0800

    [E] register

--- src/main/java/lq2007/mcmod/isaacformc/common/network/PacketRegister.java
+++ src/main/java/lq2007/mcmod/isaacformc/common/network/PacketRegister.java
@@ -1,11 +1,6 @@
 package lq2007.mcmod.isaacformc.common.network;
 
-import lq2007.mcmod.isaacformc.common.capability.CompoundNBTStorage;
-import lq2007.mcmod.isaacformc.common.capability.NoStorage;
 import lq2007.mcmod.isaacformc.register.registers.IRegister;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.INBTSerializable;
 import org.objectweb.asm.Type;
 
 public class PacketRegister implements IRegister {
@@ -17,11 +12,8 @@ public class PacketRegister implements IRegister {
         if (name.startsWith("lq2007.mcmod.isaacformc.common.network") && !name.contains("$")) {
             try {
                 Class aClass = classLoader.loadClass(name);
-                if (aClass.isInterface() && ICapabilityProvider.class.isAssignableFrom(aClass)) {
-                    // remove 'I'
-                    Class defaultInstance = classLoader.loadClass("lq2007.mcmod.isaacformc.common.capability." + aClass.getSimpleName().substring(1));
-                    Capability.IStorage storage = INBTSerializable.class.isAssignableFrom(aClass) ? CompoundNBTStorage.get() : NoStorage.get();
-                    capabilities.add(new Object[] { aClass, defaultInstance, storage });
+                if (BasePacket.class.isAssignableFrom(aClass)) {
+
                 }
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
